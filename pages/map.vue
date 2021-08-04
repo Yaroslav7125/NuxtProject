da3aa73435d4975eef5908aa15a2c41b5efa0066
Yaroslav <bahanovaroslav@gmail.com>
2021-08-04 19:02:06 +0300

    Add map page

--- pages/map.vue
+++ pages/map.vue
@@ -1,9 +1,51 @@
 <template>
-  <div>
-    <h1>Map</h1>
-  </div>
+  <div id="container" />
 </template>
 
 <script>
-export default {}
+import { load } from '@2gis/mapgl';
+export default {
+
+  data() {
+    return {
+      currentLocation: {},
+      circleOptions: {
+
+      },
+      locations: [
+        {
+          lat: 44.933076,
+          lng: 15.629058
+        },
+      ],
+      pins: {
+        selected: "data:image/png;base64,iVBORw0KGgo...",
+        notSelected: "data:image/png;base64,iVBORw0KGgo..."
+      },
+      mapStyle: [],
+      clusterStyle: [
+        {
+          url: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m1.png",
+          width: 56,
+          height: 56,
+          textColor: "#fff"
+        }
+      ]
+    }
+  },
+  mounted:function(){
+    console.log('mount')
+    navigator.geolocation.getCurrentPosition((pos)=>{
+      debugger
+      this.locations[0].lat = pos.coords.latitude;
+      this.locations[0].lng = pos.coords.longitude;
+    }, (err) => {
+      debugger
+    });
+  }
+}
 </script>
+
+<style>
+
+</style>
