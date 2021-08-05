2672e3b5533eb491a2828b6dd72606efc0efebb7
Yaroslav <bahanovaroslav@gmail.com>
2021-08-05 17:07:50 +0300

    Add  todo page and add components for todo page(AddTodo.vue, Todo.vue, TodoList.vue)

---
+++ components/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="content">
+        <Todo 
+        v-for="(todo, index) in todos"
+        v-bind:todo='todo'
+        v-bind:index='index'
+        v-on:deleteTodo="deleteTodo"
+        v-on:changeTodoTitle="changeTodoTitle"
+        v-on:ChangeTodoCompleted="ChangeTodoCompleted"
+        />
+    </div>
+</template>
+
+<script>
+import Todo from './Todo.vue'
+export default{
+    props:['todos'],
+    components:{
+        Todo:Todo
+    },
+    methods:{
+      deleteTodo(id){
+          this.$emit('deleteTodo', id);
+      },
+      changeTodoTitle(index, StrTitle){
+                                              ///
+          this.$emit('changeTodoTitle', index, StrTitle);
+      },
+      ChangeTodoCompleted(index){
+          this.$emit('ChangeTodoCompleted', index);
+      }
+    },
+}
+</script>
--- pages/todo.vue
+++ pages/todo.vue
@@ -1,9 +1,96 @@
 <template>
-  <div>
-    <h1>Todo</h1>
+  <div class="todo-content">
+    <div id="app">
+      <h1>My todo list!</h1>
+      <AddTodo
+        userInput="userInput"
+        v-on:updateUserInput  ="userInput = $event"
+        v-on:addTodo='PushTodo'
+      />
+      <TodoList
+        v-bind:todos='filteredTodos'
+        v-on:deleteTodo="deleteTodo"
+        v-on:ChangeTodoCompleted="ChangeTodoCompleted"
+        v-on:changeTodoTitle="changeTodoTitle"
+      />
+    </div>
   </div>
+
 </template>
 
 <script>
-export default {}
+import TodoList from '@/components/TodoList.vue'
+import AddTodo from '@/components/AddTodo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    AddTodo,
+  },
+  data() {
+    return {
+      todos: [
+        { id:1,title:'купить хлеп', completed: false},
+        { id:2,title:'купить матрас', completed: false},
+        { id:3,title:'купить сено', completed: false},
+      ],
+      userInput: '',
+    }
+  },
+  methods: {
+    saveTodos() {
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    },
+    PushTodo(newTodo) {
+      this.todos.push(newTodo);
+      this.saveTodos() // сетим в local Storage
+    },
+    deleteTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id);
+      this.saveTodos(); //сетим в local Storage
+    },
+    ChangeTodoCompleted(index) {
+      this.todos[index].completed = !this.todos[index].completed;
+      this.saveTodos(); // сетим при изм completed
+    },
+    changeTodoTitle(index, StrTitle){
+      this.todos[index].title = StrTitle;
+      this.saveTodos();// сетим при изменении title
+    }
+  },
+
+  computed: {
+    filteredTodos: function () {
+
+      if (this.userInput != '') {
+        return this.todos.filter(t => t.title.includes(this.userInput))
+      } else {
+        return this.todos;
+      }
+    },
+  },
+  mounted: function () {
+    {
+      if (JSON.parse(localStorage.getItem("todos"))) {
+        this.todos = JSON.parse(localStorage.getItem("todos"));// читаем
+      }
+      else localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
+  }
+}
+
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.todo-content{
+  padding-top: 24px;
+}
+</style>
