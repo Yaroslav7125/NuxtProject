dfbc0b737d80862c1a4a4849ce1b7cf31109ffbe
Yaroslav7125 <bahanovaroslav@gmail.com>
2021-08-06 00:06:33 +0300

    Add eslint with webee plagins

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,19 @@ module.exports = {
     es2021: true
   },
   extends: [
-    'plugin:vue/essential',
-    'standard'
+    'plugin:@web-bee-ru/base', // js/ts only
+
+    // npm i --save-dev eslint-plugin-vue
+    "plugin:@web-bee-ru/vue", // vue
+
+    // npm i --save-dev eslint-plugin-vue eslint-import-resolver-nuxt
+    "plugin:@web-bee-ru/nuxt", // nuxt (vue)
+
+    // npm i --save-dev eslint-plugin-react eslint-plugin-react-hooks
+    // "plugin:@web-bee-ru/react", // react
+
+    // npm i --save-dev eslint-plugin-react eslint-plugin-react-hooks
+    //"plugin:@web-bee-ru/next", // next (react)
   ],
   parserOptions: {
     ecmaVersion: 12,
