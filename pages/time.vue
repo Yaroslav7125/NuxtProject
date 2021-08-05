3624c9d0e834501778dccb60f3da38eaaa36c45d
Yaroslav <bahanovaroslav@gmail.com>
2021-08-05 13:41:53 +0300

    Add timer page.

--- pages/time.vue
+++ pages/time.vue
@@ -1,9 +1,41 @@
 <template>
   <div>
-    <h1>Time</h1>
+    <div class="timer">
+      <div class="timer-content d-flex justify-content-between">
+        <div class="timer-content-fp d-flex flex-column">
+          <span>Timer</span>
+          <span class="time-count" id="time">00:00:05</span>
+        </div>
+        <div class="timer-content-sp">
+          <img src="@/static/img/UpStroke.svg" alt="">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {}
 </script>
+<style>
+.timer{
+ padding: 24px 32px;
+}
+.timer-content{
+  border: 1px solid #DDE0E6;
+  background-color: #fff;
+  padding: 16px;
+}
+.timer-content-fp span{
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: 0.005em;
+  color: #2D2F33;
+}
+.time-count{
+  margin-top: 16px;
+}
+</style>
