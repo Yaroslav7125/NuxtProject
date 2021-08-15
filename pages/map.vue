104b7b7bb1dfdc955025367a67e66dd536c2ea8b
Yaroslav7125 <bahanovaroslav@gmail.com>
2021-08-15 19:01:49 +0300

    Add scoped to style.

--- pages/map.vue
+++ pages/map.vue
@@ -47,7 +47,7 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
 #map{
   width: 100%;
   height: 404px;
--- pages/time.vue
+++ pages/time.vue
@@ -27,7 +27,7 @@ export default {
   },
 };
 </script>
-<style>
+<style scoped>
 .timer{
  padding: 24px 32px;
 }
